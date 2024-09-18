07d7b0b1952b64f36a0eb8a07931504e0d41d0c4
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-18 21:12:36 +0200

    Header JSX

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <div className="--pad header">
+        <div className="--flex-between">
+            <h3><span className="--fw-thin">Welcome, </span> <span className="--color-danger">Harry</span></h3>
+            <button className="--btn --btn-danger">Logout</button>
+        </div>
+    </div>
   )
 }
 
