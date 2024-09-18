1f587c57b36d236422c77405b60b5e34e7662baf
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-18 20:25:03 +0200

    Register Page JSX

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import styles from './auth.module.scss';
+import Card from '../../component/card/Card';
+import { FaUserPlus } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <h1>Register</h1>
+    <div className={`container ${styles.auth}`}>
+        <Card>
+            <div className={styles.form}>
+                <div className="--flex-center">
+                    <FaUserPlus size={35} color='#999' />
+                </div>
+                <h2>Register</h2>
+                <form>
+                    <input type="text" placeholder='Name' required name='name' />
+                    <input type="email" placeholder='Email' required name='email' />
+                    <input type="password" placeholder='Password' required name='password' />
+                    <input type="password" placeholder='Confirm Password' required name='password2' />
+
+                    <buton type="submit" className="--btn --btn-primary --btn-block">Login</buton>
+                </form>
+                <Link to='/forgot'>Forgot Password</Link>
+
+                <span className={styles.register}>
+                    <Link to='/'>Home</Link>
+                    <p> &nbsp; Already have an account ? &nbsp;</p>
+                    <Link to='/login'>Login</Link>
+                </span>
+            </div>
+        </Card>
+    </div>
   )
 }
 
